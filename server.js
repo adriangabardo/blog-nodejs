1415090259ee1ae933d6a74245ce046efcdb8b70
Adrian Gabardo <adrian@fingermarkglobal.com>
2019-01-05 13:23:51 -0200

    Erro de CORS concertado no servidor

--- server.js
+++ server.js
@@ -27,6 +27,11 @@ mongoose.connect(dbConfig.url, {
 
 // Define o destino de onde os arquivos estão vindo (incluindo index.html)
 app.use(express.static('public'))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/', (req, res) =>
   res.sendFile('index.html')
