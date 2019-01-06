e7d6beeb9181b0464c5fb671ec001a5b992bdf52
Adrian Gabardo <adrian@fingermarkglobal.com>
2019-01-06 10:21:43 -0200

    Roteamento configuramento

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { TodosPostsComponent } from './todos-posts/todos-posts.component';
 
 const routes: Routes = [
-  /*{
+  {
     path: '',
+    redirectTo: '/todos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'todos',
     component: TodosPostsComponent
   },
   {
     path: 'editar/:id',
     component: PostComponent
-  }*/
+  }
 ];
 
 @NgModule({
