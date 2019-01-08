b806747cae57583427040503158b47447a78022c
Adrian Gabardo <adrian@fingermarkglobal.com>
2019-01-08 12:58:59 -0200

    CORS alterado para autorizar todos os tipos de request HTTP

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ mongoose.connect(dbConfig.url, {
 app.use(express.static('public'))
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
